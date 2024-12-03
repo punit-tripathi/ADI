ea82bbdbd8d17ab704e55bd27e38347d14a6f603
punit-tripathi <punitmani25@gmail.com>
2024-12-03 21:36:12 +0530

    G08

--- src/ADIlec20/Main.java
+++ src/ADIlec20/Main.java
@@ -1,15 +1,52 @@
 package ADIlec20;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
+    static class Edge implements Comparable<Edge> {
+        int src, dest, weight;
+
+        Edge(int src, int dest, int weight) {
+            this.src = src;
+            this.dest = dest;
+            this.weight = weight;
+        }
+
+        public int compareTo(Edge other) {
+            return this.weight - other.weight;
+        }
+    }
     public static void main(String[] args) {
 
     }
+    public static void kruskalMST(List<Edge> edges, int V) {
+        Collections.sort(edges);
+        DisjointSet ds = new DisjointSet(V);
+        int mstWeight = 0;
+        List<Edge> mstEdges = new ArrayList<>();
+
+        for (Edge edge : edges) {
+            if (ds.find(edge.src) != ds.find(edge.dest)) {
+                mstEdges.add(edge);
+                mstWeight += edge.weight;
+                ds.union(edge.src, edge.dest);
+            }
+        }
+
+        System.out.println("Total weight of MST: " + mstWeight);
+        for (Edge edge : mstEdges) {
+            System.out.println("Edge: " + edge.src + " - " + edge.dest + " with weight " + edge.weight);
+        }
+    }
+
 }
-class Disjointset{
+class DisjointSet{
     int[] parent;
     int [] rank;
 
-    Disjointset(int v){
+    DisjointSet(int v){
         parent=new int[v];
         rank=new int[v];
 
@@ -38,7 +75,6 @@ class Disjointset{
             rank[n2]++;
         }
     }
-
 
 
 }
